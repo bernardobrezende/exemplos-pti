1334c5f98adade7e1249a46c024a0e27bf27b8ef
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-10-05 14:32:14 -0300

    feat(diretivas): adicionar exemplo de bindings de isolate scope na diretiva.

--- 3-Angular/HelloWorld/hwResultadoPesquisa.js
+++ 3-Angular/HelloWorld/hwResultadoPesquisa.js
@@ -7,21 +7,26 @@ angular.module('helloWorld')
   .directive('hwResultadoPesquisa', function() {
     return {
       scope: {
-        texto: '='
+        texto: '=', // texto="var-da-scope-controller"
+        sum: '&', // gera uma função com a expressão
+        subTexto: '@' // valor literal
       },
       // A = attribute
       // E = element
       // C = class
       restrict: 'CEA',
-      template: '<h1 class="bold" ng-show="texto">{{ texto }}, Aqui vão os resultados!</h1>',
+      template: '<h1 class="bold" ng-show="texto">{{ texto }}, Aqui vão os resultados:</h1>',
       link: function(scope, elem, attrs) {
+        console.log('= %o', scope.texto);
+        console.log('& %o', scope.sum());
+        console.log('@ %o', scope.subTexto);
         elem.on('click', function() {
           //elem.html(scope.texto);
         });
-        console.log('scope: %o', scope);
+        /*console.log('scope: %o', scope);
         console.log('elem: %o', elem);
         console.log('attrs: %o', attrs);
-        console.log('opcao: %o', attrs.opcao);
+        console.log('opcao: %o', attrs.opcao);*/
       }
     };
   });
