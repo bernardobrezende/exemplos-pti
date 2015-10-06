4d3377dcac7abb354243a3f454215d39662f549c
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-10-06 08:50:53 -0300

    feat(starfoz-api): versionar api.

---
+++ 3-Angular/starfoz-api/README.md
@@ -0,0 +1,9 @@
+## Setup & Run (Local)
+
+`$ npm install`
+`$ node_modules/json-server/bin/index.js --watch db.json`
+
+## Setup & Run (Global)
+
+`$ npm install -g json-server`
+`$ json-server --watch db.json`
--- 3-Angular/starfoz/src/controllers/LoginController.js
+++ 3-Angular/starfoz/src/controllers/LoginController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-starFoz.controller('LoginController', function($scope) {
+starFoz.controller('LoginController', function($scope, $http) {
   $scope.login = function() {
     console.log($scope.user);
   };
