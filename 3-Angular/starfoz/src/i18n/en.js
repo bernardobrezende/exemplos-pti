0611556f15fe2f6ccf74d1bcd440fc510edfae1d
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2015-10-08 11:56:48 -0300

    feat(i18n): adicionar mais traduções inglês e espanhol.

--- 3-Angular/starfoz/src/i18n/en.js
+++ 3-Angular/starfoz/src/i18n/en.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var enTranslations = {
-  WELCOME: 'Welcome to',
+  WELCOME: 'Welcome to {{ ::appName }} !!',
   ENGLISH: 'English',
   SPANISH: 'Spanish',
   LOGIN: 'Login',
-  LOGOUT: 'Logout'
+  LOGOUT: 'Logout',
+  USERS: 'Users',
+  METRICS: 'Metrics',
+  TIPS: 'Tips',
+  USERNAME: 'Username',
+  PASS: 'Pass',
+  PASS_CONFIRMATION: 'Pass confirmation',
+  RESET: 'Reset',
+  ERROR_INVALID_CREDENTIALS: 'Invalid credentials!',
+  NAME: 'Name',
+  ROLE: 'Role'
 };
--- 3-Angular/starfoz/src/i18n/es.js
+++ 3-Angular/starfoz/src/i18n/es.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var esTranslations = {
-  WELCOME: 'Bienvenido a',
+  WELCOME: 'Bienvenido a {{ ::appName }} !!',
   ENGLISH: 'Ingles',
   SPANISH: 'Español',
-  LOGIN: 'Acceso',
-  LOGOUT: 'Desacceso'
+  LOGIN: 'Acceder',
+  LOGOUT: 'Desacceso',
+  USERS: 'Usuarios',
+  METRICS: 'Metricas',
+  TIPS: 'Consejos',
+  USERNAME: 'Nombre de Usuario',
+  PASS: 'Contraseña',
+  PASS_CONFIRMATION: 'Confirmación de Contraseña',
+  RESET: 'Limpiar',
+  ERROR_INVALID_CREDENTIALS: 'Credenciales invalidas!',
+  NAME: 'Nombre',
+  ROLE: 'Perfil'
 };
